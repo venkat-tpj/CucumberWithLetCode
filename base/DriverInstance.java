6e0df91831235d4def84053d7c3391a2b9e7552f
venkat-tpj <157680736+venkat-tpj@users.noreply.github.com>
2024-02-16 23:15:00 +0530

    Add files via upload
    CucumberUsingPOM

---
+++ base/DriverInstance.java
@@ -0,0 +1,12 @@
+package base;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DriverInstance {
+
+	protected RemoteWebDriver driver;
+	protected WebDriverWait wait;
+
+
+}
---
+++ base/PageContext.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageContext {
+
+	private RemoteWebDriver driver;
+	private WebDriverWait wait;
+	
+			public RemoteWebDriver getDriver() {
+				return driver;
+			}
+			public void setDriver(RemoteWebDriver driver) {
+				this.driver = driver;
+			}
+			public WebDriverWait getWait() {
+				return wait;
+			}
+			public void setWait(WebDriverWait wait) {
+				this.wait = wait;
+			}
+			
+			
+	
+}
